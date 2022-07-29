d9eec3cc2160491e2c9ec712c0531beb83837ffb
GuusjeB <32428748+GuusjeB@users.noreply.github.com>
2022-07-29 15:19:08 +0200

    Update departure.js

--- departure.js
+++ departure.js
@@ -1,7 +1,11 @@
 const subscription_key = ""
-let req = new Request("https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips?originUicCode=8400058&destinationUicCode=8400621&dateTime=2022-07-29 14:52:00");
+let req = new Request("https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips?originUicCode=8400058&destinationUicCode=8400621");
 req.method = "get";
 req.headers={"Ocp-Apim-Subscription-Key": subscription_key}
 const response = await req.loadJSON();
 
-console.log(response)
+const trips = JSON.parse(response);
+
+single_trip = Object.keys(trips)[1];
+
+console.log(single_trip)
